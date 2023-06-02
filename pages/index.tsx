bb8d237905ae633b573bde50f6eaed78d34651f6
HariChidam <hchidam@umich.edu>
2023-06-01 23:29:35 -0500

    grammar

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,9 @@ export default function Home() {
     <div className="flex justify-center items-center">
       <div className="w-2/5 mr-4 flex flex-col justify-center">
         <p className="text-lg">
-          Hi, My name is Hariharan (Hari) Chidambaram! I am a student at the University of Michigan studying Computer Science and I am particularly interested in Security Engineering and Cloud Engineering! I love Tennis and every endurance sport, especially Nordic Skiing and biking.
+          Hi, My name is Hariharan (Hari) Chidambaram! I am a student at the University of Michigan studying Computer Science!
+          I am currently a Sercurity Engineering intern at Flywheel.io. I am particularly interested in Security Engineering and Cloud Engineering! 
+          Outside of the tech world I love Tennis and every endurance sport especially Nordic Skiing!
         </p>
       </div>
       <div className="w-1/5 rounded-full overflow-hidden">
