e77f73e00ec5c1c53b1ba2f6fe7669e40e3e3a02
HariChidam <hchidam@umich.edu>
2023-06-03 22:23:20 -0500

    fixing typos

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Project from '../components/Project';
-import Future from 'components/components/Future';
+import Future from '../components/Future';
 import THT from '../public/THT.png';
 import V1 from '../public/V1.png';
 import TB from '../public/TB.png';
@@ -19,32 +19,29 @@ export default function Projects() {
         <Project
           iconSrc={THT}
           title="Theta Tau Website"
-          description="This is the platform I have built for my professional engineering fraternity!
-           Externally, it serves as an informational website. 
-           Internally, it is used to view upcoming events, track pledging progress, 
-           and provide easy access to the organization's resources."
-          learnedText = "This was my first full-stack project on my own! I learned how to design an idea using Figma and refine it through user interviews. I also learned how to connect a frontend to a backend and utilize a database (Supabase) for storing information. Additionally, I gained knowledge in implementing authentication through Google OAuth. Now, I outline new features to be added and assist other members in implementing these changes to ensure the platform continues to be used and maintained after my graduation!"
-          techUsed='React, TailwindCSS, Supabase, Netlify, Figma'
+          description="This is the platform I have built for my professional engineering fraternity! Externally, it serves as an informational website. Internally, it is used to view upcoming events, track pledging progress, and provide easy access to the organization's resources."
+          learnedText="This was my first full-stack project on my own! I learned how to design an idea using Figma and refine it through user interviews. I also learned how to connect a frontend to a backend and utilize a database (Supabase) for storing information. Additionally, I gained knowledge in implementing authentication through Google OAuth. Now, I outline new features to be added and assist other members in implementing these changes to ensure the platform continues to be used and maintained after my graduation!"
+          techUsed="React, TailwindCSS, Supabase, Netlify, Figma"
           buttonText="Check it out!"
-          buttonLink='https://thetatau-umich.org/'
+          buttonLink="https://thetatau-umich.org/"
         />
         <Project
           iconSrc={V1}
           title="V1 Platform"
-          description="This is the V1 Plaform! This is where the V1 community can come to fine a one stop shop for everything startups related. From a newsetter to a page with all the startups and foudners ahd UMich to a dashbaord of all the events we do and the resources from them!"
-          learnedText = "This was my first time wowrking on a project with many other engineers and managing a team. I personally developed the dashboard page and \startups page! I am now the platform lead and have learned alot about diving up a feature into tasks on Jira and working with a team of mixed level engineers to built a product! I have also learned a lot about writing clean code and reusing components as much as possible. We have over 2000 users and are growing everyday!"
-          techUsed='React, Next JS, TailwindCSS, Railway, Supabase, Netlify'
+          description="This is the V1 Platform! This is where the V1 community can come to find a one-stop-shop for everything startup-related. From a newsletter to a page with all the startups and founders at UMich, to a dashboard of all the events we do and the resources from them!"
+          learnedText="This was my first time working on a project with many other engineers and managing a team. I personally developed the dashboard page and startups page! I am now the platform lead and have learned a lot about dividing up a feature into tasks on Jira and working with a team of mixed-level engineers to build a product! I have also learned a lot about writing clean code and reusing components as much as possible. We have over 2000 users and are growing every day!"
+          techUsed="React, Next JS, TailwindCSS, Railway, Supabase, Netlify"
           buttonText="Check it out!"
-          buttonLink='https://v1michigan.com/'
+          buttonLink="https://v1michigan.com/"
         />
         <Project
           iconSrc={TB}
           title="TableVision"
-          description="This is a state-of-the-art product for students to find study spots. I built a custom CV model to accuraletly tell if tables are occupied or not from security camera footage and then dispaly the number of open tables in each building. I am currenlty working with the school to implement TableVison at UMich!"
-          learnedText='This was my first time working with Computer Vision and I learned a lot about the process of building a model. I learned how to use YoloV5 and Roboflow to create and train a model and then how to deploy it using Flask and Next JS. This was my first time using Flask and I learned how to create my own API and use it to make requests to my custom Yolov5 model. I am now managing a team of 4 to help me implement TableVision at UMich!'
-          techUsed='React, Next JS, TailwindCSS, Flask, YoloV5, Roboflow, Google Colab, Netlify'
+          description="This is a state-of-the-art product for students to find study spots. I built a custom CV model to accurately tell if tables are occupied or not from security camera footage and then display the number of open tables in each building. I am currently working with the school to implement TableVision at UMich!"
+          learnedText="This was my first time working with Computer Vision, and I learned a lot about the process of building a model. I learned how to use YOLOv5 and Roboflow to create and train a model and then how to deploy it using Flask and Next JS. This was my first time using Flask, and I learned how to create my own API and use it to make requests to my custom YOLOv5 model. I am now managing a team of 4 to help me implement TableVision at UMich!"
+          techUsed="React, Next JS, TailwindCSS, Flask, YOLOv5, Roboflow, Google Colab, Netlify"
           buttonText="Check it out!"
-          buttonLink='https://pitch.com/public/d8126c2f-9acb-48a5-bb90-d086556c168d'
+          buttonLink="https://pitch.com/public/d8126c2f-9acb-48a5-bb90-d086556c168d"
         />
         <Future
           iconSrc={blueline}
@@ -58,4 +55,5 @@ export default function Projects() {
     </div>
   );
 }
+
 
