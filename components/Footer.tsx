64af7e0b9a4d59a1a56407fab28ad2e626cb7e60
HariChidam <hchidam@umich.edu>
2024-01-17 17:54:50 -0500

    year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
       <div className="flex items-center float-left justify-start">
         <a className="flex items-center float-left justify-start" href= "https://www.youtube.com/watch?v=9ak9Uxtntfk">
         <Image src={Michigan} alt="Michigan Logo" className="h-6 w-6 mr-2" />
-        <span className="font-bold text-blue-1000">Class of &apos;24</span> 
+        <span className="font-bold text-blue-1000">Class of &apos;25</span> 
         </a>
       </div>
       <div className="flex items-center float-right justify-end space-x-2">
